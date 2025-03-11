e786bb1e95afb144e82ed47bd2212878d9c549fe
Shayan <nwryshayan500@gmail.com>
2025-03-11 19:44:07 +0330

    meals Database added

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,6 +1,7 @@
 import MealsHeader from '@/components/meals/meals-header/meals-header';
-import styles from './page.module.css'
 import MealsGrid from '@/components/meals/meals-grid/meals-grid';
+
+import styles from './page.module.css'
 
 export default function MealsPage() {
   return (
